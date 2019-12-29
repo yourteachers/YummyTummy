bbdd7a96eeb8eda8281f90ea293115692ac42765
Julian <j.srouji95@gmail.com>
2019-12-29 12:58:37 +0200

    ADDED BLUETOOTH

--- YummyTummy_v02/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ YummyTummy_v02/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f08004b;
-        public static final int end = 0x7f08007e;
-        public static final int gone = 0x7f080090;
-        public static final int invisible = 0x7f08009e;
-        public static final int left = 0x7f0800a3;
-        public static final int packed = 0x7f0800e1;
-        public static final int parent = 0x7f0800e3;
-        public static final int percent = 0x7f0800ea;
-        public static final int right = 0x7f0800f0;
-        public static final int spread = 0x7f080118;
-        public static final int spread_inside = 0x7f080119;
-        public static final int start = 0x7f08011e;
-        public static final int top = 0x7f080143;
-        public static final int wrap = 0x7f080155;
+        public static final int bottom = 0x7f080051;
+        public static final int end = 0x7f080084;
+        public static final int gone = 0x7f080096;
+        public static final int invisible = 0x7f0800a4;
+        public static final int left = 0x7f0800a9;
+        public static final int packed = 0x7f0800e7;
+        public static final int parent = 0x7f0800e9;
+        public static final int percent = 0x7f0800f0;
+        public static final int right = 0x7f0800f6;
+        public static final int spread = 0x7f08011f;
+        public static final int spread_inside = 0x7f080120;
+        public static final int start = 0x7f080125;
+        public static final int top = 0x7f08014a;
+        public static final int wrap = 0x7f08015d;
     }
     public static final class styleable {
         private styleable() {}
--- YummyTummy_v02/app/src/main/java/com/example/yummytummy_v01/Connect.java
+++ YummyTummy_v02/app/src/main/java/com/example/yummytummy_v01/Connect.java
@@ -2,19 +2,66 @@ package com.example.yummytummy_v01;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.sax.StartElementListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
+import app.akexorcist.bluetotohspp.library.DeviceList;
+
+import static app.akexorcist.bluetotohspp.library.BluetoothState.DEVICE_OTHER;
+import static app.akexorcist.bluetotohspp.library.BluetoothState.REQUEST_CONNECT_DEVICE;
+import static app.akexorcist.bluetotohspp.library.BluetoothState.STATE_CONNECTED;
 
 public class Connect extends Fragment {
 
     private ConnectViewModel mViewModel;
+    Button BTconnect;
+    Button startAll;
+    Button startNoHeat;
+    TextInputEditText water;
+    TextInputEditText scoops;
+    TextInputLayout waterInputLayout;
+    TextInputLayout scoopsInputLayout;
+    BluetoothSPP bluetooth;
+    final String activateMachineNoHeat="fn";
+    final String activateMachine="f";
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if(requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
+            if(resultCode == Activity.RESULT_OK) {
+
+                bluetooth.setupService();
+                bluetooth.startService(DEVICE_OTHER);
+                bluetooth.connect(intent);
+                Toast.makeText(getActivity(), "request code = connect device", Toast.LENGTH_SHORT).show();
+            }
+        } else if(requestCode == BluetoothState.REQUEST_ENABLE_BT) {
+            if(resultCode == Activity.RESULT_OK) {
+                bluetooth.setupService();
+                bluetooth.startService(DEVICE_OTHER);
+                Toast.makeText(getActivity(), "request code = enable bt", Toast.LENGTH_SHORT).show();
+
+            } else {
+                // Do something if user doesn't choose any device (Pressed back)
+            }
+        }
+    }
 
     public static Connect newInstance() {
         return new Connect();
@@ -23,7 +70,152 @@ public class Connect extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.connect_fragment, container, false);
+        View root = inflater.inflate(R.layout.connect_fragment, container, false);
+        BTconnect = root.findViewById(R.id.BTconnect);
+        startAll = root.findViewById(R.id.StartAll);
+        startNoHeat = root.findViewById(R.id.StartNoHeat);
+        water = root.findViewById(R.id.Water);
+        scoops = root.findViewById(R.id.Scoops);
+        waterInputLayout = root.findViewById(R.id.watertextInputLayout);
+        scoopsInputLayout = root.findViewById(R.id.scoopstextInputLayout);
+        bluetooth = new BluetoothSPP(getActivity());
+        //startAll.setEnabled(false);
+        //startNoHeat.setEnabled(false);
+
+        bluetooth.setBluetoothStateListener(new BluetoothSPP.BluetoothStateListener() {
+            public void onServiceStateChanged(int state) {
+                if (state == BluetoothState.STATE_CONNECTED) {
+                    Toast.makeText(getActivity(), "Bluetooth is connected", Toast.LENGTH_SHORT).show();
+                }
+                // Do something when successfully connected
+                else if (state == BluetoothState.STATE_CONNECTING) {
+                    Toast.makeText(getActivity(), "Bluetooth is connecting", Toast.LENGTH_SHORT).show();
+                }
+                // Do something while connecting
+                else if (state == BluetoothState.STATE_LISTEN) {
+                    Toast.makeText(getActivity(), "Bluetooth is listening", Toast.LENGTH_SHORT).show();
+                }
+                // Do something when device is waiting for connection
+                else if (state == BluetoothState.STATE_NONE) {
+                    Toast.makeText(getActivity(), "Bluetooth is doing nothing", Toast.LENGTH_SHORT).show();
+                }
+                // Do something when device don't have any connection
+            }
+        });
+
+        bluetooth.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() {
+            public void onDeviceConnected(String name, String address) {
+                BTconnect.setText("Connected to " + name);
+                startAll.setEnabled(true);
+                startNoHeat.setEnabled(true);
+            }
+
+            public void onDeviceDisconnected() {
+                BTconnect.setText("Connection lost");
+                startAll.setEnabled(false);
+                startNoHeat.setEnabled(false);
+            }
+
+            public void onDeviceConnectionFailed() {
+                BTconnect.setText("Unable to connect");
+            }
+
+        });
+
+        bluetooth.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() {
+            public void onDataReceived(byte[] data, String message) {
+                // Do something when data incoming
+                Toast.makeText(getActivity(), "recieved sometthing", Toast.LENGTH_SHORT).show();
+               // tempText.setText(message);
+            }
+        });
+        BTconnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bluetooth.isBluetoothAvailable()) {
+
+                    if (bluetooth.getServiceState() == STATE_CONNECTED) {
+                        bluetooth.disconnect();
+                        BTconnect.setText("CONNECT VIA BLUETOOTH");
+                    } else {
+                        Intent intent = new Intent(getActivity(), DeviceList.class);
+                        startActivityForResult(intent, REQUEST_CONNECT_DEVICE);
+
+
+                    }
+
+                    //finish();
+                } else {
+                    Toast.makeText(getActivity(), "Bluetooth is not available", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        startAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bluetooth.getServiceState() == STATE_CONNECTED) {
+                    String waterAmount = water.getText().toString();
+                    String scoopsAmount = scoops.getText().toString();
+
+                    if(validate(waterAmount,scoopsAmount)){
+                        bluetooth.send(activateMachine+","+waterAmount+","+scoopsAmount, true);
+                    }
+
+
+
+
+                } else {
+                    Toast.makeText(getActivity(), "Device not connected", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+        startNoHeat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bluetooth.getServiceState() == STATE_CONNECTED) {
+                    String waterAmount = water.getText().toString();
+                    String scoopsAmount = scoops.getText().toString();
+
+                    if(validate(waterAmount,scoopsAmount)){
+                        bluetooth.send(activateMachineNoHeat+","+waterAmount+","+scoopsAmount, true);
+                    }
+
+
+
+
+                } else {
+                    Toast.makeText(getActivity(), "Device not connected", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+        return root;
+    }
+    private boolean validate(String waterAmount,String scoopsAmount){
+        if(waterAmount.length()==0){
+            waterInputLayout.setError("Enter amount of water between 100-500 mL");
+            scoopsInputLayout.setError(null);
+            return false;
+        }
+        if(scoopsAmount.length()==0){
+            scoopsInputLayout.setError("Enter amount of scoops between 1-4");
+            waterInputLayout.setError(null);
+            return false;
+        }
+        int wAmount = Integer.getInteger(waterAmount);
+        int sAmount = Integer.getInteger(scoopsAmount);
+        if(wAmount>500 || wAmount<100){
+            waterInputLayout.setError("Enter amount of water between 100-500 mL");
+            scoopsInputLayout.setError(null);
+            return false;
+        }
+        if(sAmount<1 || sAmount>4){
+            scoopsInputLayout.setError("Enter amount of scoops between 1-4");
+            waterInputLayout.setError(null);
+            return false;
+        }
+        return true;
     }
 
     @Override
